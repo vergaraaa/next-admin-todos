9030ab32a0d226f5795541d789e207acfa218387
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-15 20:23:15 +0200

    ✨ update cart items

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,6 +1,25 @@
-import { CiMenuBurger, CiSearch, CiChat1, CiBellOn } from "react-icons/ci";
+import { cookies } from "next/headers";
+import {
+  CiMenuBurger,
+  CiSearch,
+  CiChat1,
+  CiShoppingCart,
+} from "react-icons/ci";
 
 export const TopMenu = () => {
+  const cookieStore = cookies();
+  const cart = JSON.parse(cookieStore.get("cart")?.value ?? "{}");
+
+  const getTotalCount = () => {
+    let items = 0;
+
+    Object.values(cart).forEach((value) => {
+      items += value as number;
+    });
+
+    return items;
+  };
+
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
       <div className="px-6 flex items-center justify-between space-x-4">
@@ -32,8 +51,12 @@ export const TopMenu = () => {
           <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             <CiChat1 size={25} />
           </button>
-          <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-            <CiBellOn size={25} />
+          <button className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+            <span className="text-sm mr-2 font-bold text-blue-800">
+              {getTotalCount()}
+            </span>
+
+            <CiShoppingCart size={25} />
           </button>
         </div>
       </div>
--- src/products/components/ProductCard.tsx
+++ src/products/components/ProductCard.tsx
@@ -1,8 +1,10 @@
-// https://tailwindcomponents.com/component/e-commerce-product-card
+"use client";
 
 import Image from "next/image";
 import { IoAddCircleOutline, IoTrashOutline } from "react-icons/io5";
 import { Star } from "./Star";
+import { addProductToCart } from "@/shopping-cart/actions/actions";
+import { useRouter } from "next/navigation";
 
 interface Props {
   id: string;
@@ -13,6 +15,13 @@ interface Props {
 }
 
 export const ProductCard = ({ id, name, price, rating, image }: Props) => {
+  const router = useRouter();
+
+  const onAddToCart = () => {
+    addProductToCart(id);
+    router.refresh();
+  };
+
   return (
     <div className="bg-white shadow rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-100">
       {/* Product Image */}
@@ -33,6 +42,7 @@ export const ProductCard = ({ id, name, price, rating, image }: Props) => {
             {name}
           </h3>
         </a>
+
         <div className="flex items-center mt-2.5 mb-5">
           {/* Stars */}
           {Array(rating)
@@ -54,7 +64,10 @@ export const ProductCard = ({ id, name, price, rating, image }: Props) => {
           </span>
 
           <div className="flex">
-            <button className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            <button
+              onClick={onAddToCart}
+              className="text-white mr-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
               <IoAddCircleOutline size={25} />
             </button>
             <button className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
