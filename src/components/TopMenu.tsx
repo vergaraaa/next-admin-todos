08e724106840f9c9f989fea41ff686c881a47639
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-16 05:53:56 +0200

    ✨ show products in cart

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,4 +1,5 @@
 import { cookies } from "next/headers";
+import Link from "next/link";
 import {
   CiMenuBurger,
   CiSearch,
@@ -53,7 +54,10 @@ export const TopMenu = () => {
           <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
             <CiChat1 size={25} />
           </button>
-          <button className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+          <Link
+            href="/dashboard/cart"
+            className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200"
+          >
             {totalItems > 0 && (
               <span className="text-sm mr-2 font-bold text-blue-800">
                 {totalItems}
@@ -61,7 +65,7 @@ export const TopMenu = () => {
             )}
 
             <CiShoppingCart size={25} />
-          </button>
+          </Link>
         </div>
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/todos/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/products/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shopping-cart/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
