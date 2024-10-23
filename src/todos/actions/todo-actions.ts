00fe8fb459e052ed67c22d0cbe457567a320f708
Edgar Vergara <edgarvm13@hotmail.com>
2024-10-23 20:28:24 +0200

    ✨ get users todos

--- src/todos/actions/todo-actions.ts
+++ src/todos/actions/todo-actions.ts
@@ -28,9 +28,9 @@ export const toggleTodo = async (
   return updatedTodo;
 };
 
-export const addTodo = async (description: string) => {
+export const addTodo = async (description: string, userId: string) => {
   try {
-    const todo = await prisma.todo.create({ data: { description } });
+    const todo = await prisma.todo.create({ data: { description, userId } });
 
     revalidatePath("/dashboard/server-todos");
 
